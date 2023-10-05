9d3a08794e6ec7445d223d428a12b00dfc1b874d
karanb141 <karanb141@gmail.com>
2023-10-05 17:36:13 -0500

    updated PCtf2.1

--- main.tf
+++ main.tf
@@ -12,9 +12,9 @@ terraform {
     storage_account_name = "pcapistorage"
     container_name       = "tfstatefile"
     key                  = "terraform.tfstate"
-    access_key           = "eX3RHJAzsN15k64psjYas/0bwIYimtaOE3k9CKXoa/DRZ13EoC4h8ftp6s9wy4NT0+p+nkhAEje/+AStnHczWQ=="
+    #access_key           = "eX3RHJAzsN15k64psjYas/0bwIYimtaOE3k9CKXoa/DRZ13EoC4h8ftp6s9wy4NT0+p+nkhAEje/+AStnHczWQ=="
     #tenant_id = "15d51b4e-b761-428f-b55c-5030645146f6"
-    subscription_id      = "c3ace9c5-f5d5-491f-9c11-ac9c41516a41"
+    #subscription_id      = "c3ace9c5-f5d5-491f-9c11-ac9c41516a41"
     use_oidc             = true
   }
 }
