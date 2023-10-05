66b4e8543236f407b49ae0c06b0125faf4732610
karanb141 <karanb141@gmail.com>
2023-10-05 16:45:51 -0500

    updated PCtf1.4

--- main.tf
+++ main.tf
@@ -8,9 +8,9 @@ terraform {
 
   # Update this block with the location of your terraform state file
   backend "azurerm" {
-    resource_group_name  = "rg-terraform-github-actions-state"
-    storage_account_name = "terraformgithubactions"
-    container_name       = "tfstate"
+    resource_group_name  = "TerraformPC_API_update"
+    storage_account_name = "pcapistorage"
+    container_name       = "tfstatefile"
     key                  = "terraform.tfstate"
     #tenant_id = "15d51b4e-b761-428f-b55c-5030645146f6"
     use_oidc             = true
@@ -23,7 +23,3 @@ provider "azurerm" {
 }
 
 # Define any Azure resources to be created here. A simple resource group is shown here as a minimal example.
-resource "azurerm_resource_group" "rg-aks" {
-  name     = var.resource_group_name
-  location = var.location
-}
